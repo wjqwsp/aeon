2ce4a7f1931f33630ec3e1af7ffc060a74fa8923
王嘉盛 <wangjiasheng@bupt.edu.cn>
2018-04-30 20:45:00 +0800

    debug

--- src/runtime_types.erl
+++ src/runtime_types.erl
@@ -7,7 +7,7 @@
 
 -define(FUN_NAME, '#types').
 
--define(dbg(DbgMsg, Args), lager:info(DbgMsg, Args)).
+-define(dbg(DbgMsg, Args), io:format(DbgMsg, Args)).
 %%-define(dbg(DbgMsg, Args), noop).
 
 parse_transform(Forms, Options) ->
